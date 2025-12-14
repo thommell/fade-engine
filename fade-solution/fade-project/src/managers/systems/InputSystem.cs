64c2a605ac0acc16965103f940bcc041eba67d82
thommell <thomslaghekke@outlook.com>
2025-12-14 21:05:06 +0100

    EP-2 Added small interface for the input system

---
+++ fade-solution/fade-project/src/managers/systems/InputSystem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace fade_project.systems;
+
+public sealed class InputSystem {
+    private MouseState _mouseState;
+    private KeyboardState _kbState;
+    
+    //TODO: Later?
+    // private MouseState _previousMouseState;
+    // private KeyboardState _previousKbState;
+    
+    public void Update(GameTime gameTime) {
+        _kbState = Keyboard.GetState();
+        _mouseState = Mouse.GetState();
+    }
+
+    public bool IsKeyUp(Keys key) => _kbState.IsKeyUp(key);
+    public bool IsKeyPressed(Keys key) => _kbState.IsKeyDown(key);
+
+    public bool IsMouseLeftUp() => _mouseState.LeftButton == ButtonState.Pressed;
+    public bool IsMouseMiddleUp() => _mouseState.MiddleButton == ButtonState.Pressed;
+    public bool IsMouseRightUp() => _mouseState.RightButton == ButtonState.Pressed;
+}
