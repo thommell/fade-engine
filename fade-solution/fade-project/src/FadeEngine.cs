ecb64965897f578e45ddef89694584c86a2b29d3
thommell <thomslaghekke@outlook.com>
2025-12-14 19:21:48 +0100

    EP-1 Added FadeEngine wrapper
    Game loop abstraction added

---
+++ fade-solution/fade-project/src/FadeEngine.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace fade_project;
+
+public sealed class FadeEngine {
+    // MonoGame
+    private SpriteBatch _spriteBatch;
+    private ContentManager _content;
+    public void Initialize() {}
+    public void Load(SpriteBatch spriteBatch, ContentManager content) {
+        _spriteBatch = spriteBatch;
+        _content = content;
+    }
+    public void Unload() {}
+    public void Update(GameTime gameTime) {}
+    public void Draw(SpriteBatch spriteBatch) {
+    }
+}
