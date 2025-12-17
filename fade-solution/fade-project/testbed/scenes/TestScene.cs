659aff5eca7e2845eb9f15b8d9f4ea4ab323f5de
thommell <thomslaghekke@outlook.com>
2025-12-17 17:52:55 +0100

    Added more interfaces to use for input

---
+++ fade-solution/fade-project/testbed/scenes/TestScene.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using fade_project.containers;
+using Microsoft.Xna.Framework;
+
+namespace fade_project.testbed.scenes;
+
+public class TestScene : Scene {
+    public override void OnEnter() {
+        Console.WriteLine("Test!");
+        base.OnEnter();
+    }
+
+    public override void OnExit() {
+        Console.WriteLine("LEAVING");
+        base.OnExit();
+    }
+}
